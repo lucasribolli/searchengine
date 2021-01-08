5aa2eabaff6a05ad1d4f3c30241c4512515820cb
LucasBlasquez <lucasribolli@gmail.com>
2021-01-08 20:25:14 -0300

    add /api/search to general searchs

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 # searchengine
 Search engine using Scrapy Framework to get web data, Elasticsearch to store it, Flask to create APIs and JavaScript to search and visualizate it.
 
+## Python setup
+```
+py -3 -m venv virtualenv
+.\virtualenv\Scripts\Activate.ps1
+pip install -r requirements.txt
+```
+
 ## Run Scrapy
 ```
-cd crawler/wikipedia
-```
-```
+cd crawler\wikipedia
 scrapy crawl wikipedia -O data\wikipedia.json
+```
+
+## Elasticsearch
+Please install 7.10.1 elasticsearch version and run it through `.bat` file
+```
+C:\elasticsearch-7.10.1\bin\elasticsearch.bat
 ```
 
 ## Run index_data.py
 Be careful to not run twice.
 ```
-cd ..
-```
-```
-cd ..
-```
-```
+cd ..\..
 python index_data.py
 ```
+
--- backend/server.py
+++ backend/server.py
@@ -1,43 +1,31 @@
 from flask import Flask, jsonify, request
 from ES import ES
+from elasticsearch_dsl import Q, A
 from Wikipedia import Wikipedia
+
 
 app = Flask(__name__)
 ES()
 
-@app.route('/getalltext', methods=["GET"])
-def getalltext():
+@app.route('/search', methods=["GET"])
+def search():
+    search = request.json["search"]
+    start = request.args.get("start", type=int, default=0)
+    end = request.args.get("end", type=int, default=10)
     wikipedia = Wikipedia()
     s = wikipedia.search()
-    s = s.query("match_all")
+    q = Q("multi_match", 
+            query=search, 
+            fields=["title", "text"])
+    s = s.query(q)
+    #s = s.sort('-lastmod')
     data = []
-    for hit in s:
+    for hit in s[start:end]:
         data.append({
-            "url": hit.url,
-            "title": hit.title,
-            "lastmod": hit.lastmod,
-            "text": hit.text
-        })
-    return jsonify({
-        "data": data,
-        "total": s.count()
-    })
-
-@app.route('/getbytitle', methods=["GET"])
-def get_by_title():
-    title = request.args.get("title", 
-                            type=str,
-                            default="Python")
-    wikipedia = Wikipedia()
-    s = wikipedia.search()
-    s = s.query("match", title=title)
-    data = []
-    for hit in s:
-        data.append({
-            "url": hit.url,
-            "title": hit.title,
-            "lastmod": hit.lastmod,
-            "text": hit.text
+            "url": hit.url if hasattr(hit, "url") else "",
+            "title": hit.title if hasattr(hit, "title") else "",
+            "lastmod": hit.lastmod if hasattr(hit, "lastmod") else "",
+            "text": hit.text if hasattr(hit, "text") else ""
         })
     return jsonify({
         "data": data,
